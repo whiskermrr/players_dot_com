6aa61a7c45120c7a15ae69d67caa80d9e07b66f1
mario <mariusz-perkowski@wp.pl>
2017-11-09 23:11:32 +0100

    porawki w panelu administracyjnym

--- competition/admin.py
+++ competition/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
-from competition.models import *
+from .models import *
 
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'sname', 'age', 'team')
+
+
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ('host', 'guest', 'date', 'hostGoals', 'guestGoals')
+
+
+class KolejkaAdmin(admin.ModelAdmin):
+    list_display = ('name', 'match')
+
+
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('name', 'name1', 'points')
+
+
+class MatchFactsAdmin(admin.ModelAdmin):
+    list_display = ('match', 'player', 'incident', 'minute')
+
+
+admin.site.register(Player, PlayerAdmin)
 admin.site.register(LeagueType)
-admin.site.register(Match)
-admin.site.register(Kolejka)
+admin.site.register(Match, MatchAdmin)
+admin.site.register(Kolejka, KolejkaAdmin)
 admin.site.register(Team)
-admin.site.register(Table)
-admin.site.register(Player)
-admin.site.register(MatchFacts)
+admin.site.register(Table, TableAdmin)
+admin.site.register(MatchFacts, MatchFactsAdmin)
