2aa19d0f8b48fd25e1061d63d0e9623a714bedea
mario <mariusz-perkowski@wp.pl>
2017-11-10 10:33:41 +0100

    poprawka config

--- players_dot_com/settings.py
+++ players_dot_com/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'competition.apps.MusicConfig',
+    'competition.apps.CompetitionConfig',
 ]
 
 MIDDLEWARE = [
