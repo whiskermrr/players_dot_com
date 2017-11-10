1df2b2674e8302f361bbe68ff74b2f349bf77d7d
mario <mariusz-perkowski@wp.pl>
2017-11-10 10:18:18 +0100

    dodanie lokalizacji na media

--- players_dot_com/settings.py
+++ players_dot_com/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'competition',
+    'competition.apps.MusicConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'competition/templates'),)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
