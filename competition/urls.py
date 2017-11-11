64cc959e24e673bdddf7e833b2467791b8c13d5f
mario <mariusz-perkowski@wp.pl>
2017-11-11 18:33:35 +0100

    dodano url CRUD player

--- competition/urls.py
+++ competition/urls.py
@@ -13,7 +13,13 @@ urlpatterns = [
     #players /competition/player
     url(r'^player/$', views.player, name='player'),
     # players /competition/player/8/
-    url(r'^player/(?P<pk>[0-9]+)/$', views.player, name='player_details'),
+    url(r'^player/(?P<player_id>[0-9]+)/$', views.player_details, name='player_details'),
+    # players /competition/player/delete/8/
+    url(r'^player/(?P<player_id>[0-9]+)/delete/$', views.player_delete, name='player_delete'),
+    # players /competition/player/update/8/
+    url(r'^player/(?P<player_id>[0-9]+)/update/$', views.player_update, name='player_update'),
+    # player /competition/player/add
+    url(r'^player/add/$', views.player_add, name='player-add'),
 
     # TEAM URLS
     #team /competition/team
